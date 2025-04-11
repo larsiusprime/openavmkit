d24f38f8745882e8b5103b4618e3502f269725c5
gregmiller <millergregm22@gmail.com>
2025-04-11 11:13:12 -0400

    correct columns names

--- tests/test_data.py
+++ tests/test_data.py
@@ -588,9 +588,9 @@ def test_spatial_inference():
 	mask_infer = mask_eligible & mask_needs_inference & mask_has_proxy
 	df_expected.loc[mask_infer, "size"] = df_expected.loc[mask_infer, "true"]
 	
-	# Set up inference flags
-	df_expected["size_inferred"] = False
-	df_expected.loc[mask_infer, "size_inferred"] = True
+	# Set up inference flags - NOTE: Changed column name to match implementation
+	df_expected["inferred_size"] = False
+	df_expected.loc[mask_infer, "inferred_size"] = True
 
 	# Run inference
 	df = _do_perform_spatial_inference(df, s_infer, "size", "key", verbose=True)
@@ -601,9 +601,9 @@ def test_spatial_inference():
 
 	# Add debug prints
 	print("\nExpected DataFrame:")
-	print(df_expected[["hood", "size", "size_inferred"]])
+	print(df_expected[["hood", "size", "inferred_size"]])
 	print("\nActual DataFrame:")
-	print(df[["hood", "size", "size_inferred"]])
+	print(df[["hood", "size", "inferred_size"]])
 
 	assert dfs_are_equal(df, df_expected, primary_key="key")
 
