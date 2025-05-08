3895ec5f115aa966f71e21dd8e4a3225d6e99c22
Lars Doucet <lars.doucet@gmail.com>
2025-05-08 17:50:18 -0500

    add simple_mra, update simple_ols, add scatterplot

--- openavmkit/utilities/plotting.py
+++ openavmkit/utilities/plotting.py
@@ -1,6 +1,66 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import colorsys
+
+def get_nice_random_colors(n: int):
+  """
+  Generate a list of n aesthetically pleasing random colors for plotting
+  """
+  colors = []
+  for i in range(n):
+    h = i / n
+
+    # pick a nice saturation value, don't use the full range, just an aesthetic choice:
+    s = 0.75 + 0.25 * np.sin(i / n * 2 * np.pi)
+
+    # pick a nice value, don't use the full range, just an aesthetic choice:
+    v = 0.75 + 0.25 * np.cos(i / n * 2 * np.pi)
+
+    # convert from hsv to rgb:
+    r, g, b = colorsys.hsv_to_rgb(h, s, v)
+
+    hex_code = f"#{int(r * 255):02x}{int(g * 255):02x}{int(b * 255):02x}"
+
+    colors.append(hex_code)
+  return colors
+
+
+def plot_scatterplot(
+    df: pd.DataFrame,
+    x: str,
+    y: str,
+    xlabel: str = "",
+    ylabel: str = "",
+    title: str = "",
+    x_lim: tuple = None,
+    y_lim: tuple = None,
+    out_file: str = None,
+    style: dict = None
+):
+  plt.close('all')
+
+  color = None
+  if style is not None:
+    if "random_color_by" in style:
+      random_color_by = style.get("random_color_by", "")
+      if random_color_by in df:
+        unique_values = df[random_color_by].unique()
+        colors = get_nice_random_colors(len(unique_values))
+        color_map = dict(zip(unique_values, colors))
+        color = df[random_color_by].map(color_map).values
+
+  plt.scatter(df[x], df[y], alpha=0.25, c=color)
+  plt.xlabel(xlabel)
+  plt.ylabel(ylabel)
+  plt.title(title)
+  if x_lim is not None:
+    plt.xlim(x_lim[0], x_lim[1])
+  if y_lim is not None:
+    plt.ylim(y_lim[0], y_lim[1])
+  if out_file is not None:
+    plt.savefig(out_file)
+  plt.show()
 
 
 def plot_histogram_df(df: pd.DataFrame, fields: list[str], xlabel: str = "", ylabel: str = "", title: str = "", bins = 500, x_lim=None, out_file: str = None):
