ada4b55fdd0f1b328206ac9e5b751622b95c8fe8
gregmiller <139252038+gregmiller00@users.noreply.github.com>
2025-05-09 14:23:00 -0700

    Update timing.py
    back to old again

--- openavmkit/utilities/timing.py
+++ openavmkit/utilities/timing.py
@@ -1,17 +1,17 @@
+import time
+
+
 class TimingData:
 	_data = {}
 	results = {}
-
-	def __init__(self):
+	@@ -9,28 +6,23 @@
 		self._data = {}
 		self.results = {}
-
 	def start(self, key):
 		if key in self.results:
 			self._data[key] = time.time() - self.results[key]
 		else:
 			self._data[key] = time.time()
-
 	def stop(self, key):
 		if key in self._data:
 			result = time.time() - self._data[key]
@@ -19,10 +19,13 @@ class TimingData:
 			return result
 		else:
 			return -1
-
 	def get(self, key):
 		return self.results.get(key)
 
-
 	def print(self):
 		value = ""
+		for key in self.results:
+			if value != "":
+				value += "\n"
+			value += (f"{key}: {self.results[key]:.2f} seconds")
+		return value
