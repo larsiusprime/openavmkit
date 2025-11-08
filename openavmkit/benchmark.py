ecafd8a347a6bd77610dfa8ff8f0ead999140237
Lars Doucet <lars.doucet@gmail.com>
2025-11-07 21:29:59 -0600

    update benchmark use of shaps

--- openavmkit/benchmark.py
+++ openavmkit/benchmark.py
@@ -60,7 +60,6 @@ from openavmkit.modeling import (
     write_model_parameters
 )
 from openavmkit.reports import MarkdownReport, _markdown_to_pdf
-from openavmkit.shap_analysis import quick_shap
 from openavmkit.time_adjustment import enrich_time_adjustment
 from openavmkit.utilities.data import (
     div_df_z_safe,
@@ -2052,11 +2051,9 @@ def _write_model_results(results: SingleModelResults, outpath: str, settings: di
     with open(f"{path}/pred_universe.pkl", "wb") as f:
         pickle.dump(results.pred_univ, f, protocol=pickle.HIGHEST_PROTOCOL)
     
-    params_path = f"{path}/params"
+    params_path = f"{path}"
     
-    write_model_parameters(results.model, results, params_path, "test")
-    write_model_parameters(results.model, results, params_path, "sales")
-    write_model_parameters(results.model, results, params_path, "universe")
+    write_model_parameters(results.model, results, params_path)
 
 
 def _write_ensemble_model_results(
@@ -3306,7 +3303,7 @@ def _model_shaps(model_group: str, all_results: MultiModelResults, title: str):
     for key in all_results.model_results:
         smr: SingleModelResults = all_results.model_results[key]
         _title = f"{title}/{model_group}/{key}"
-        quick_shap(smr, True, _title)
+        _quick_shap(smr, True, _title)
 
 
 def _get_earliest_and_latest_date(df: pd.DataFrame):
@@ -4373,3 +4370,38 @@ def _collect_base_model_predictions(
                 template_ds = smr.ds
 
     return predictions, true_values, template_ds
+
+
+def _quick_shap(
+    smr: SingleModelResults, 
+    plot: bool = False, 
+    title: str = ""
+):
+    """
+    Compute SHAP values for a given model and dataset and optionally plot it.
+
+    Parameters
+    ----------
+    smr : SingleModelResults
+        The SingleModelResults object containing the fitted model and data splits.
+    plot : bool, optional
+        If True, generate and display a SHAP summary plot. Defaults to False.
+    title : str, optional
+        Title to use for the SHAP plot if `plot` is True. Defaults to an empty string.
+
+    Returns
+    -------
+    np.ndarray
+        SHAP values array for the evaluation dataset.
+    """
+
+    if smr.type not in ["xgboost", "catboost", "lightgbm"]:
+        # SHAP is not supported for this model type
+        return
+
+    X_train = smr.ds.X_train
+
+    shaps = _calc_shap(smr.model, X_train, X_train)
+
+    if plot:
+        plot_full_beeswarm(shaps, title=title)
