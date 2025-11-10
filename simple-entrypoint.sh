e297f17cce2a803956908313b3a5eca601ec35cb
Jackson Arnold <jackson.n.arnold@gmail.com>
2025-11-10 12:27:09 -0500

    Automated CI end-to-end test of notebooks using docker container (#206)
    * Setup and testing changes
    
    * Revert "Setup and testing changes"
    
    This reverts commit 4613ea4a33e1d63aed1a5fa4b535a83c0f0bf4ee.
    
    * Create .python-version
    
    * Delete .python-version
    
    * Created dockerfile
    
    * Update dockerfile
    
    * Update dockerfile
    
    * Remove jupyter host renaming
    
    * Test ci to ghcr
    
    * Change image platform
    
    * Update build workflows
    
    * Add multi-arch emulation
    
    * Update action trigger and tagging
    
    * Fix trigger name
    
    * Fix typo
    
    * Try new tags
    
    * Update tagging
    
    * Add dockerignore
    
    * Change docker ignore
    
    * Fix image issues
    
    * Fix jupyter homepage config
    
    * Fix dockerfile syntax
    
    * Fix string syntax
    
    * Fix syntax
    
    * Fix syntax
    
    * Clean code
    
    * Fix 404 on python kernel in docker container
    
    * Test docs deployment
    
    * Start multi-stage build dockerfile
    
    * Add process_dataframes to pipeline.py
    
    * Fix output column size and failed writes
    
    * Fix crs and add try catch
    
    * run pytest
    
    * fix indentation
    
    * Add ci e2e test of notebooks
    
    * Move test to separate job
    
    * Add all notebooks
    
    * Make test job dependent
    
    * Revert notebook 1 for PR
    
    * Change notebooks to test
    
    ---------
    
    Co-authored-by: Lars Doucet <lars.doucet@gmail.com>

---
+++ simple-entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# If any of the jupyter execute commands fail, exit the script
+set -e
+
+if [ "$RUN" = "test" ]; then
+    # Create /app/notebooks/pipeline/data/us-nc-guilford/ directory
+    mkdir -p /app/notebooks/pipeline/data/us-nc-guilford/
+
+    # Create the cloud.json file in the /app/notebooks/pipeline/data/us-nc-guilford/ directory
+    echo '{
+        "type": "azure",
+        "azure_storage_container_url": "https://landeconomics.blob.core.windows.net/localities-public"
+    }' > /app/notebooks/pipeline/data/us-nc-guilford/cloud.json
+
+    echo "--- Starting Notebook Test Run ---"
+    
+    echo "Running: 01-assemble.ipynb"
+    jupyter execute notebooks/pipeline/01-assemble.ipynb
+    
+    echo "Running: 02-clean.ipynb"
+    jupyter execute notebooks/pipeline/02-clean.ipynb
+    
+    echo "Running: 03-model.ipynb"
+    jupyter execute notebooks/pipeline/03-model.ipynb
+
+    # Notebooks 04 and 05 to be added when they are complete
+
+    echo "Running: assessment-quality.ipynb"
+    jupyter execute notebooks/pipeline/assessment-quality.ipynb
+    
+    echo "--- All notebooks ran successfully ---"
+else
+    # Expose the notebooks file with jupyter notebook on container start
+    # IP 0.0.0.0 sets it to be accessed external to the container
+    # Allow root allows it to modify the file structure and volume
+    # --no-browser avoids opening the browser automatically, as there is not one in the container
+    exec jupyter notebook --ip 0.0.0.0 --allow-root --no-browser  
+fi
