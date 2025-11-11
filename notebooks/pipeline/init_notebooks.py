0380f1fd45324d25515a00f1f97893ddb288069c
Jackson Arnold <jackson.n.arnold@gmail.com>
2025-11-11 09:40:01 -0500

    Add setting locality through env var

--- notebooks/pipeline/init_notebooks.py
+++ notebooks/pipeline/init_notebooks.py
@@ -10,3 +10,26 @@ def setup_environment():
         sys.path.insert(0, repo_root)
     print("Environment setup completed.")
 
+
+def check_for_different_locality(locality: str):
+    """
+    Checks the "LOCALITY" environmental variable to see if its set. Returns it if it is, otherwise returns the passed in slug.
+
+    Attributes
+    ----------
+    locality : str
+        The locality slug from the notebook (e.g., "us-nc-guilford").
+
+    Returns
+    -------
+    locality : str
+        The new locality slug (e.g., "us-nc-guilford").
+    """
+
+    new_locality = os.getenv("LOCALITY")
+
+    if new_locality:
+        print("Found new locality: " + new_locality)
+        return new_locality
+    else:
+        return locality
